3805dae2e52c90fd17ad39f51d68f826a1a1d023
Rynco Maekawa <lynzrand@outlook.com>
2025-02-03 22:21:38 +0800

    refactor: directly return byte buffer for name

--- crates/monoxide-ttf/src/model/mod.rs
+++ crates/monoxide-ttf/src/model/mod.rs
@@ -9,13 +9,6 @@ pub mod cmap;
 
 /// The trait implemented by all tables in a TrueType font file.
 pub trait ITable {
-    fn name_raw(&self) -> &'static str;
+    fn name(&self) -> &'static [u8; 4];
     fn write(&self, writer: &mut impl BufMut);
-
-    fn name(&self) -> [u8; 4] {
-        assert!(self.name_raw().len() == 4);
-        let mut name = [0u8; 4];
-        name.copy_from_slice(self.name_raw().as_bytes());
-        name
-    }
 }
