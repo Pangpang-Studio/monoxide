80217a5cfdb2c06c1a200b71ba07e7c38ab9ab35
Rynco Maekawa <lynzrand@outlook.com>
2025-02-13 21:07:18 +0800

    tool: Add more verify methods to the verify script

--- tools/ttf-verify.py
+++ tools/ttf-verify.py
@@ -4,6 +4,7 @@ This script is used to verify the correctness of TTF files.
 It uses the FontTools library to parse the TTF file and check for any errors.
 """
 
+from io import BytesIO
 from fontTools.ttLib import TTFont
 import argparse
 from pathlib import Path
@@ -20,20 +21,31 @@ def main():
         print(f"File {file} does not exist.")
         exit(1)
 
+    phase = "read"
     try:
         font = TTFont(file)
+        print("Read the TTF file successfully.")
 
         # Dump info using xml. The process read all data in the file,
         # so it will fail if the file is corrupted.
-        out_xml = file.with_suffix(".xml")
+        phase = "ttf->xml"
+        out_xml = file.with_suffix(".ttx")
         font.saveXML(out_xml)
 
-        print(f"File {file} is a valid TTF file.")
         print(f"Dumped XML to {out_xml}.")
+
+        # Convert the XML back to TTF for an additional round of error checking.
+        phase = "xml->ttf"
+        font = TTFont()
+        font.importXML(out_xml)
+        font.save(BytesIO())
+        print("Converted XML back to TTF.")
+
+        print(f"File {file} is a valid TTF file.")
     except Exception as e:
         import traceback
 
-        print(f"Error verifying file {file}: {e}")
+        print(f"Error verifying file {file} ({phase}): {e}")
         traceback.print_exc()
 
         print("The font file generated is malformed.")
