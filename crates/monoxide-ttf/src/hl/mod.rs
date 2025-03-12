96e375919cc52bd53b7df6fbde346e4fc46aa9f9
Rynco Maekawa <lynzrand@outlook.com>
2025-03-12 22:25:56 +0800

    feat: Add quad curve to outline converter

--- crates/monoxide-ttf/src/hl/mod.rs
+++ crates/monoxide-ttf/src/hl/mod.rs
@@ -3,5 +3,6 @@
 //! These data structures will be mapped to the low-level structures in
 //! [`crate::model`] so that they can be written to the binary format.
 pub mod cmap;
+pub mod glyf;
 pub mod loca;
 pub mod maxp;
