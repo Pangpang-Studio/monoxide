de4400b2c6d8de8ec7b9f921b40f903b38ccd012
Rynco Maekawa <lynzrand@outlook.com>
2025-04-05 21:17:25 +0800

    feat: add `monoxide` JS module

--- crates/monoxide-script/src/js/mod.rs
+++ crates/monoxide-script/src/js/mod.rs
@@ -1,18 +1,11 @@
-use rquickjs::{Class, Ctx, Object};
+use rquickjs::{Class, Ctx, JsLifetime, Object};
+
+use crate::FontParamSettings;
 
 mod bezier_builder;
 mod glyph_factory;
 mod outline_expr;
 mod spiro_builder;
-
-pub fn import_globals<'js>(cx: Ctx<'js>, global_object: Object<'js>) -> rquickjs::Result<()> {
-    let factory = glyph_factory::GlyphFactory::new(cx)?;
-    global_object.set("glyph", factory)?;
-    global_object.set("bezier", js_declare_bezier)?;
-    global_object.set("spiro", js_declare_spiro)?;
-
-    Ok(())
-}
 
 #[rquickjs::function]
 fn declare_bezier(
@@ -25,5 +18,97 @@ fn declare_bezier(
 
 #[rquickjs::function]
 fn declare_spiro(cx: Ctx<'_>) -> rquickjs::Result<Class<'_, spiro_builder::SpiroBuilder>> {
-    Class::instance(cx, spiro_builder::SpiroBuilder::new())
+    spiro_builder::SpiroBuilder::new(cx)
+}
+
+/// The struct to attach to the Monoxide-enabled QuickJS runtime. Contains data
+/// for the rest of the program to evaluate.
+#[derive(Clone, JsLifetime)]
+pub struct ContextAttachment<'js> {
+    pub font_params: FontParamSettings,
+    factory: Class<'js, glyph_factory::GlyphFactory>,
+}
+
+impl std::fmt::Debug for ContextAttachment<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let factory = self.factory.borrow();
+
+        f.debug_struct("ContextAttachment")
+            .field("font_params", &self.font_params)
+            .field("factory", &*factory)
+            .finish()
+    }
+}
+
+impl<'js> ContextAttachment<'js> {
+    pub fn new(cx: Ctx<'js>, params: FontParamSettings) -> rquickjs::Result<Self> {
+        let globals = &cx.globals();
+        Class::<glyph_factory::GlyphFactory>::define(globals)?;
+        Class::<glyph_factory::SimpleGlyphBuilder>::define(globals)?;
+        Class::<bezier_builder::BezierBuilder>::define(globals)?;
+        Class::<spiro_builder::SpiroBuilder>::define(globals)?;
+        Class::<outline_expr::OutlineExprObject>::define(globals)?;
+
+        let factory = glyph_factory::GlyphFactory::new(cx.clone())?;
+
+        Ok(ContextAttachment {
+            font_params: params,
+            factory,
+        })
+    }
+}
+
+/// Represents a JS Module within the
+pub struct MonoxideModule;
+
+impl rquickjs::module::ModuleDef for MonoxideModule {
+    fn declare(decl: &rquickjs::module::Declarations<'_>) -> rquickjs::Result<()> {
+        decl.declare("glyph")?;
+        decl.declare("bezier")?;
+        decl.declare("spiro")?;
+        decl.declare("settings")?;
+        Ok(())
+    }
+
+    fn evaluate<'js>(
+        cx: &Ctx<'js>,
+        exports: &rquickjs::module::Exports<'js>,
+    ) -> rquickjs::Result<()> {
+        let ud = cx
+            .userdata::<ContextAttachment<'js>>()
+            .expect("Cannot find user adata");
+
+        exports.export("glyph", ud.factory.clone())?;
+        exports.export("bezier", js_declare_bezier)?;
+        exports.export("spiro", js_declare_spiro)?;
+
+        let settings = Object::new(cx.clone())?;
+        settings.prop("width", ud.font_params.width)?;
+        settings.prop("xHeight", ud.font_params.x_height)?;
+        settings.prop("descender", ud.font_params.descender)?;
+        settings.prop("capHeight", ud.font_params.cap_height)?;
+        exports.export("settings", settings)?;
+
+        Ok(())
+    }
+}
+
+pub fn insert_globals<'js>(cx: Ctx<'js>) -> rquickjs::Result<()> {
+    let globals = cx.globals();
+    let ud = cx
+        .userdata::<ContextAttachment<'js>>()
+        .expect("Cannot find user adata");
+
+    globals.set("glyph", ud.factory.clone())?;
+    globals.set("bezier", js_declare_bezier)?;
+    globals.set("spiro", js_declare_spiro)?;
+
+    let settings = Object::new(cx.clone())?;
+    settings.prop("width", ud.font_params.width)?;
+    settings.prop("xHeight", ud.font_params.x_height)?;
+    settings.prop("descender", ud.font_params.descender)?;
+    settings.prop("capHeight", ud.font_params.cap_height)?;
+    globals.set("settings", settings)?;
+
+    Ok(())
 }
