761172c17c1c6463241c7e23a26516be81fbaa0c
Rynco Maekawa <lynzrand@outlook.com>
2025-04-06 17:07:35 +0800

    feat: Expose eval::eval_outline

--- crates/monoxide-script/src/eval.rs
+++ crates/monoxide-script/src/eval.rs
@@ -1,5 +1,11 @@
-use crate::ast::FontContext;
+use monoxide_curves::{point::Point2D, CubicBezier};
+
+use crate::ast::{FontContext, OutlineExpr};
 
 pub fn eval(cx: &FontContext) -> monoxide_ttf::model::FontFile {
+    todo!()
+}
+
+pub fn eval_outline(expr: &OutlineExpr, out: &mut Vec<CubicBezier<Point2D>>) {
     todo!()
 }
