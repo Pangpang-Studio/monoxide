06510a9c9ff5ab61415cfbfeade845cc8674a7ef
Rynco Maekawa <lynzrand@outlook.com>
2025-04-07 22:39:50 +0800

    feat(playground): Add support for loading from file

---
+++ font/index.js
@@ -0,0 +1,6 @@
+import { bezier, spiro, settings, glyph } from 'monoxide'
+
+let g = glyph.simple((b) => {
+  b.add(bezier(0.3, 0).lineTo(0.3, 0).lineTo(1, settings.width).lineTo(0.3, 0).build())
+})
+glyph.assignChar(g, 'c')
