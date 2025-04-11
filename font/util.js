902697e9efc37643be89d8c74c8a49401830149a
rami3l <rami3l@outlook.com>
2025-04-11 21:28:19 +0800

    feat(font): refine `util`

--- font/util.js
+++ font/util.js
@@ -1,4 +1,4 @@
-import mx from 'monoxide'
+import * as mx from 'monoxide'
 
 /** @typedef {{ meth: string; args: number[] }} Inst */
 
@@ -20,10 +20,10 @@ import mx from 'monoxide'
 
 /**
  * @param {() => any} builderFactory
- * @returns {(insts: Inst[]) => import('monoxide').OutlineExpr}
+ * @returns {(...insts: Inst[]) => import('monoxide').OutlineExpr}
  */
 function instCollector(builderFactory) {
-  return (insts) => {
+  return (...insts) => {
     const builder = builderFactory()
     for (const { meth, args } of insts) builder[meth](...args)
     return builder.build()
@@ -31,13 +31,33 @@ function instCollector(builderFactory) {
 }
 
 /**
+ * @typedef {(...args: number[]) => Inst} InstFactory
+ * @param {string} meth
+ * @returns {InstFactory}
+ */
+const instFactory =
+  (meth) =>
+  (...args) => ({ meth, args })
+
+/**
  * @param {SpiroInst[]} insts
  * @returns {import('monoxide').OutlineExpr}
  */
-const spiro = instCollector(mx.spiro)
+export const spiro = instCollector(mx.spiro)
+
+export const corner = instFactory('corner')
+export const g4 = instFactory('g4')
+export const g2 = instFactory('g2')
+export const flat = instFactory('flat')
+export const curl = instFactory('curl')
+export const anchor = instFactory('anchor')
+export const handle = instFactory('handle')
+export const end = instFactory('end')
+export const open = instFactory('open')
+export const endOpen = instFactory('endOpen')
 
 /**
- * @typedef {'lineTo' | 'curveTo' | 'close'} BezierMeth
+ * @typedef {'moveTo' | 'lineTo' | 'curveTo' | 'close'} BezierMeth
  *
  * @typedef {Inst & { meth: BezierMeth }} BezierInst
  */
@@ -46,4 +66,9 @@ const spiro = instCollector(mx.spiro)
  * @param {BezierInst[]} insts
  * @returns {import('monoxide').OutlineExpr}
  */
-const bezier = instCollector(() => mx.bezier)
+export const bezier = instCollector(() => ({ moveTo: mx.bezier }))
+
+export const moveTo = instFactory('moveTo')
+export const lineTo = instFactory('lineTo')
+export const curveTo = instFactory('curveTo')
+export const close = instFactory('close')
