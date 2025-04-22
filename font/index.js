b64a7e5d3a8b00d3f99917d5d08ffd9126d6176e
rami3l <rami3l@outlook.com>
2025-04-22 13:46:48 +0800

    feat(font): add `i` example glyph

--- font/index.js
+++ font/index.js
@@ -1,11 +1,13 @@
 import { glyph } from 'monoxide'
 import { S, s } from './glyph/s'
 import { c } from './glyph/c'
+import { i } from './glyph/i'
 import { o } from './glyph/o'
 
 const glyphs = {
   S,
   c,
+  i,
   o,
   s,
 }
