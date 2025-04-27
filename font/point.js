c9b884bcfdd5515e67b29913f622b39948d72bf7
rami3l <rami3l@outlook.com>
2025-04-27 21:38:22 +0800

    refactor(font): extract the `point` module

---
+++ font/point.js
@@ -0,0 +1,13 @@
+/** @typedef {[number, number]} Point */
+
+/**
+ * The standard directions.
+ *
+ * @type {Record<string, Point>}
+ */
+export const DIR = {
+  L: [-1, 0],
+  R: [1, 0],
+  U: [0, 1],
+  D: [0, -1],
+}
