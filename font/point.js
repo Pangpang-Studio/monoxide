0eea79e7f0c049466bf7905dc9afa5a6ad1b3005
rami3l <rami3l@outlook.com>
2025-05-01 22:02:15 +0800

    refactor(font): extract `const`

--- font/point.js
+++ font/point.js
@@ -1,18 +1,6 @@
 /** @typedef {[number, number]} Point */
 
 /**
- * The standard directions.
- *
- * @type {Record<string, Point>}
- */
-export const DIR = {
-  L: [-1, 0],
-  R: [1, 0],
-  U: [0, 1],
-  D: [0, -1],
-}
-
-/**
  * @param {Point} xy
  * @param {Point} zw
  * @returns {Point}
