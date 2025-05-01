51f64af33ab112712a648ef728c278231d1c9088
rami3l <rami3l@outlook.com>
2025-05-01 21:48:02 +0800

    refactor(font): use `[-descender, ascender]` as y range in JS

--- font/glyph/i.js
+++ font/glyph/i.js
@@ -3,26 +3,19 @@ import { settings } from 'monoxide'
 import { corner, endOpen, open, simpleGlyph, spiro } from '../outline'
 import { ringAt } from '../shape'
 
-/** @import {Point} from '../point' */
-
-const { width, descender, xHeight } = settings
+const { width, xHeight, capHeight } = settings
 const mid = 0.5 * width
 const strokeW = 0.17 * width
 const halfStrokeW = 0.5 * strokeW
 
-/** @type {Point} */
-const iStrokeBot = [mid, descender]
-
-/** @type {Point} */
-const iStrokeTop = [mid, xHeight]
-
-const iStrokeHeight = iStrokeTop[1] - iStrokeBot[1]
+const strokeH = xHeight
+const dotR = 0.65 * strokeW
 
 export const i = simpleGlyph(
   spiro(
-    open(mid, descender),
-    corner(mid, xHeight - halfStrokeW),
-    endOpen(mid - 0.4 * iStrokeHeight, xHeight - halfStrokeW),
+    open(mid, 0),
+    corner(mid, strokeH - halfStrokeW),
+    endOpen(mid - 0.4 * strokeH, xHeight - halfStrokeW),
   ).stroked(strokeW),
-  ringAt(mid, 1.225 * xHeight, 0.65 * strokeW),
+  ringAt(mid, capHeight - dotR, dotR),
 )
--- font/point.js
+++ font/point.js
@@ -11,3 +11,30 @@ export const DIR = {
   U: [0, 1],
   D: [0, -1],
 }
+
+/**
+ * @param {Point} xy
+ * @param {Point} zw
+ * @returns {Point}
+ */
+export const addp = ([x, y], [z, w]) => [x + z, y + w]
+
+/**
+ * @param {Point} xy
+ * @param {number} dx
+ * @returns {Point}
+ */
+export const addx = ([x, y], dx) => [x + dx, y]
+
+/**
+ * @param {Point} xy
+ * @param {number} dy
+ * @returns {Point}
+ */
+export const addy = ([x, y], dy) => [x, y + dy]
+
+/**
+ * @param {Point} xy
+ * @returns {Point}
+ */
+export const negp = ([x, y]) => [-x, -y]
