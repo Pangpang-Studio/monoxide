e1cd7983c6a0f9e35e4c303e225a0263190ee120
rami3l <rami3l@outlook.com>
2025-05-01 23:07:40 +0800

    feat(font): add example glyph `o`

--- font/glyph/o.js
+++ font/glyph/o.js
@@ -1,5 +1,30 @@
-import { g4, simpleGlyph, spiro } from '../outline'
+import { MID, OVRS, WIDTH, XH } from '../const'
+import { curl, flat, g2, g4, simpleGlyph, spiro } from '../outline'
+
+const strokeW = 0.17 * XH
+
+const XW = 0.57 * WIDTH
+const halfXW = 0.5 * XW
+const midCurveW = 0.3 * XW
+
+const halfXH = 0.5 * XH
+const midCurveH = (1 / 17) * halfXH
+const endCurveH = (9 / 17) * halfXH
+
+const endN = g2
+const midN = g4
 
 export const o = simpleGlyph(
-  spiro(g4(0, 0.5), g4(0.5, 0), g4(1, 0.5), g4(0.5, 1)),
+  spiro(
+    midN(MID - midCurveW, midCurveH),
+    endN(MID, -OVRS),
+    midN(MID + midCurveW, midCurveH),
+    flat(MID + halfXW, endCurveH),
+    curl(MID + halfXW, XH - endCurveH),
+    midN(MID + midCurveW, XH - midCurveH),
+    endN(MID, XH + OVRS),
+    midN(MID - midCurveW, XH - midCurveH),
+    flat(MID - halfXW, XH - endCurveH),
+    curl(MID - halfXW, endCurveH),
+  ), // .stroked(strokeW),
 )
