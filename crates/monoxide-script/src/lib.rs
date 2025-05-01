a7f72b6971269b42cf3e879f1a9ea496b5e3c5be
rami3l <rami3l@outlook.com>
2025-05-01 20:25:22 +0800

    fix(playground): adjust metrics

--- crates/monoxide-script/src/lib.rs
+++ crates/monoxide-script/src/lib.rs
@@ -4,6 +4,28 @@ pub mod js;
 
 use serde::{Deserialize, Serialize};
 
+/// Defines the basic em parameters for the font.
+///
+/// The em square in this project is defined as follows:
+///
+/// ```txt
+///              y
+///    ascender _|________|
+///  cap_height _|        |
+///    x_height _| ||  || |
+///             _| ||==|| |
+///           0 _|_||__||_|____ x
+///  -descender _|________| width
+/// ```
+///
+/// In other words, in the **scripting language** (but not in the
+/// `monoxide-script` APIs), a regular glyph must be designed to
+/// follow the below coordinate restrictions:
+///
+/// - 0 <= x <= width
+/// - -descender <= y <= ascender := 1 - descender
+///
+/// See: <http://designwithfontforge.com/en-US/The_EM_Square.html>
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct FontParamSettings {
     pub width: f64,
