b225194d3c198e22d343eb33d1ee8a34488361d2
rami3l <rami3l@outlook.com>
2025-05-01 23:43:27 +0800

    refactor(font/glyph/o): extract `oShape()`

--- font/glyph/o.js
+++ font/glyph/o.js
@@ -5,26 +5,37 @@ const strokeW = 0.17 * XH
 
 const XW = 0.57 * WIDTH
 const halfXW = 0.5 * XW
-const midCurveW = 0.3 * XW
-
 const halfXH = 0.5 * XH
-const midCurveH = (1 / 17) * halfXH
-const endCurveH = (9 / 17) * halfXH
 
 const endN = g2
 const midN = g4
 
-export const o = simpleGlyph(
-  spiro(
-    midN(MID - midCurveW, midCurveH),
-    endN(MID, -OVRS),
-    midN(MID + midCurveW, midCurveH),
-    flat(MID + halfXW, endCurveH),
-    curl(MID + halfXW, XH - endCurveH),
-    midN(MID + midCurveW, XH - midCurveH),
-    endN(MID, XH + OVRS),
-    midN(MID - midCurveW, XH - midCurveH),
-    flat(MID - halfXW, XH - endCurveH),
-    curl(MID - halfXW, endCurveH),
-  ), // .stroked(strokeW),
-)
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} rx
+ * @param {number} ry
+ * @returns {import('monoxide').OutlineExpr}
+ */
+function oShape(x, y, rx, ry) {
+  const midCurveW = 0.6 * rx
+  const midCurveH = (1 / 17) * ry
+  const endCurveH = (9 / 17) * ry
+  const yLo = y - ry
+  const yHi = y + ry
+
+  return spiro(
+    midN(x - midCurveW, yLo + midCurveH),
+    endN(x, yLo - OVRS),
+    midN(x + midCurveW, yLo + midCurveH),
+    flat(x + rx, yLo + endCurveH),
+    curl(x + rx, yHi - endCurveH),
+    midN(x + midCurveW, yHi - midCurveH),
+    endN(x, yHi + OVRS),
+    midN(x - midCurveW, yHi - midCurveH),
+    flat(x - rx, yHi - endCurveH),
+    curl(x - rx, yLo + endCurveH),
+  ).stroked(strokeW)
+}
+
+export const o = simpleGlyph(oShape(MID, halfXH, halfXW, halfXH))
