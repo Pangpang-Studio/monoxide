bff48f4c28eb2604c1dc50be45cfe73111cae367
Rynco Maekawa <lynzrand@outlook.com>
2025-05-04 11:09:18 +0800

    feat: Fetch metadata in webui

--- tools/playground/src/lib/api.ts
+++ tools/playground/src/lib/api.ts
@@ -1,6 +1,18 @@
+import type { GlyphDetail } from './types'
+
 export async function ping() {
   let res = await fetch('/api/ping')
   if (!res.ok) {
     throw new Error('Failed to ping API')
   }
+}
+
+export async function getGlyphDetail(id: number) {
+  let res = await fetch(`/api/glyph/${id}`)
+  if (!res.ok) {
+    let body = await res.text()
+    throw new Error(`Failed to fetch glyph detail: ${res.statusText}; ${body}`)
+  }
+  let data = await res.json()
+  return data as GlyphDetail
 }
--- tools/playground/src/pages/Glyph.vue
+++ tools/playground/src/pages/Glyph.vue
@@ -2,45 +2,89 @@
 import { useRoute } from 'vue-router'
 import NavBar from '../components/NavBar.vue'
 import { useAppState } from '../lib/state'
-import { computed, ref } from 'vue'
+import { computed, ref, watch, type Ref } from 'vue'
+import type { GlyphDetail } from '../lib/types'
+import { getGlyphDetail } from '../lib/api'
 
 const route = useRoute()
 const state = useAppState()
 
-const glyphId = ref(parseInt(route.params.glyphId as string, 10) || 0)
+interface Props {
+  glyphId: number | string
+}
 
-const glyph = computed(() => {
+let props = defineProps<Props>()
+
+const glyphId = ref(
+  typeof props.glyphId === 'string' ? parseInt(props.glyphId) : props.glyphId,
+)
+
+const overviewGlyph = computed(() => {
   if (!state.value.renderedFont) return null
   let renderedFont = state.value.renderedFont
 
   return renderedFont.glyphs[glyphId.value]
 })
+
+// the detail we fetch from the server
+const glyphDetail: Ref<GlyphDetail | undefined> = ref()
+const error: Ref<string | null> = ref(null)
+
+watch(
+  glyphId,
+  async (newGlyphId) => {
+    try {
+      if (!state.value.renderedFont) return
+      let id = newGlyphId
+      let detail = await getGlyphDetail(id)
+      if (detail) {
+        if (glyphId.value === id) {
+          glyphDetail.value = detail
+        } else {
+          console.warn(
+            `Glyph detail for ${id} was fetched, but the glyphId changed to ${glyphId.value}. Skipping update.`,
+          )
+        }
+      }
+    } catch (e) {
+      console.error('Error fetching glyph detail:', e)
+      if (e instanceof Error) {
+        error.value = e.message
+      } else {
+        error.value = 'Unknown error'
+      }
+    }
+  },
+  { immediate: true, deep: true },
+)
 </script>
 
 <template>
   <NavBar></NavBar>
   <div
-    class="flex flex-col md:flex-row min-h-screen mx-8 my-8 gap-8"
-    v-if="glyph"
+    class="grid grid-cols-1 md:grid-cols-3 min-h-screen mx-8 my-8 gap-8"
+    v-if="overviewGlyph"
   >
     <!-- Glyph display area -->
-    <div class="flex flex-col md:flex-basis-0 md:flex-grow-2">
+    <div class="md:col-span-2">
       <svg
-        class="width-full aspect-square border-2 border-black bg-white"
+        class="w-full aspect-square border-2 border-black bg-white"
         viewBox="-0.5 -1.5 2 2"
         xmlns="http://www.w3.org/2000/svg"
       >
         <path
-          :d="glyph.svg"
+          :d="overviewGlyph.svg"
           class="stroke-black fill-gray-300 stroke-[0.005]"
+          fill-rule="nonzero"
         />
         <!-- TODO: Display debug points -->
       </svg>
     </div>
 
     <!-- Metadata & control area -->
-    <div class="flex flex-col md:flex-basis-0 md:flex-grow-1">
+    <div class="md:col-span-1 overflow-scroll">
       <h1 class="text-2xl font-bold mt-2 mb-2">glyph #{{ glyphId }}</h1>
+      <pre>{{ glyphDetail }}</pre>
     </div>
   </div>
   <div v-else class="flex flex-col items-center justify-center h-screen">
