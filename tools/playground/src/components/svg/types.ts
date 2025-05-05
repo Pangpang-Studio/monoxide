6a45a52b3af3c2de863d3d40a91e99fde6fce0e7
Rynco Maekawa <lynzrand@outlook.com>
2025-05-05 16:55:34 +0800

    feat(playground): Migrate to scaled canvas & add guidelines

---
+++ tools/playground/src/components/svg/types.ts
@@ -0,0 +1,31 @@
+export interface SvgGuideline {
+  pos: number
+  label: string | null
+}
+
+export interface SvgGuidelines {
+  h: SvgGuideline[]
+  v: SvgGuideline[]
+}
+
+export interface DraftToSvgXform {
+  scaleX: number
+  scaleY: number
+  translateX: number
+  translateY: number
+}
+
+export interface SvgGuidelinesProps extends SvgGuidelines {
+  xform: DraftToSvgXform
+  canvasWidth: number
+  canvasHeight: number
+}
+
+export interface SvgCanvasProps {
+  guidelines: { h: SvgGuideline[]; v: SvgGuideline[] }
+  /**
+   * 1-based margin on each sides. If not supplied, defaults to 0.2. Margins are
+   * relative to the extremeties of the guidelines.
+   */
+  margin?: number
+}
