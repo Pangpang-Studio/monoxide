16a1998b697ffff8bde0e122d7459a0421f449b4
Rynco Maekawa <lynzrand@outlook.com>
2025-05-05 10:23:52 +0800

    fix(playground): Fix styling, remove redundant height

--- tools/playground/src/components/GlyphDisplay.vue
+++ tools/playground/src/components/GlyphDisplay.vue
@@ -1,21 +1,22 @@
 <template>
-  <RouterLink :to="`/glyph/${props.id}`">
-    <div class="flex flex-col gap-2 p-2 hover:bg-gray-100">
-      <svg
-        class="size-40 border-2 border-black bg-white"
-        viewBox="-0.5 -1.5 2 2"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path :d="props.path" style="fill: black" fill-rule="nonzero" />
-      </svg>
-      <div class="flex flex-col">
-        <h3 class="font-bold" v-if="props.name">{{ props.name }}</h3>
-        <div v-for="(c, i) in chars_list" :key="i" class="">
-          {{ c.unicode }}
-          <pre class="inline">'{{ props.chars[i] }}'</pre>
-        </div>
-        <div v-if="!chars_list">(no chars maps to this glyph)</div>
+  <RouterLink
+    :to="`/glyph/${props.id}`"
+    class="flex flex-col gap-2 p-2 hover:bg-gray-100"
+  >
+    <svg
+      class="size-40 border-2 border-black bg-white"
+      viewBox="-0.5 -1.5 2 2"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path :d="props.path" style="fill: black" fill-rule="nonzero" />
+    </svg>
+    <div class="flex flex-col">
+      <h3 class="font-bold" v-if="props.name">{{ props.name }}</h3>
+      <div v-for="(c, i) in chars_list" :key="i" class="">
+        {{ c.unicode }}
+        <pre class="inline">'{{ props.chars[i] }}'</pre>
       </div>
+      <div v-if="!chars_list">(no chars maps to this glyph)</div>
     </div>
   </RouterLink>
 </template>
