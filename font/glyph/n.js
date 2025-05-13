e6850bb5ca85818b9491ce0237de515e325550a9
rami3l <rami3l@outlook.com>
2025-05-13 16:07:38 +0200

    fix(font/glyph/n): optimize shape

--- font/glyph/n.js
+++ font/glyph/n.js
@@ -1,19 +1,10 @@
-import { MID, OVRS, SBL, SBR, XH } from '../const'
-import {
-  corner,
-  curl,
-  endOpen,
-  flat,
-  g2,
-  open,
-  simpleGlyph,
-  spiro,
-} from '../outline'
+import { DIR, MID, OVRS, SBL, SBR, XH } from '../const'
+import { curl, endOpen, g4, open, simpleGlyph, spiro } from '../outline'
 
 // TODO: Extract this.
 const strokeW = 0.17 * XH
 
-const endN = g2
+const endN = g4
 
 export const n = (() => {
   const sbl1 = SBL + strokeW / 2
@@ -21,7 +12,6 @@ export const n = (() => {
 
   const midCurveW = 0.2 * (SBR - SBL)
   const ry = (XH - strokeW) / 2
-  const midCurveH = (1 / 17) * ry
   const endCurveH = (9 / 17) * ry
   const yHi = XH / 2 + ry
 
@@ -29,10 +19,9 @@ export const n = (() => {
     spiro(
       open(sbr1, 0),
       curl(sbr1, yHi - endCurveH),
-      endN(MID + midCurveW, yHi - midCurveH),
-      flat(MID - midCurveW, yHi + OVRS),
-      corner(sbl1, yHi + OVRS),
-      endOpen(sbl1, 0),
+      endN(MID + midCurveW, yHi + OVRS / 2),
+      endOpen(sbl1 + strokeW / 2, yHi - strokeW / 2).heading(...DIR.L),
     ).stroked(strokeW),
+    spiro(open(sbl1, 0), endOpen(sbl1, XH)).stroked(strokeW),
   )
 })()
