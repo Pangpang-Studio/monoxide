406ef7536d6c39a8c0c1b5d7a600df7630d675cb
rami3l <rami3l@outlook.com>
2025-06-14 14:29:00 +0800

    feat(font): add helper functions for `math` and `shape`

--- crates/monoxide-font/src/font/glyph/c.rs
+++ crates/monoxide-font/src/font/glyph/c.rs
@@ -2,16 +2,17 @@ use std::sync::Arc;
 
 use monoxide_script::{
     ast::{FontContext, SimpleGlyph},
-    dsl::{BezierBuilder, BezierInst},
+    dsl::BezierBuilder,
+    line,
 };
 
 pub fn c(fcx: &FontContext) -> SimpleGlyph {
     SimpleGlyph::new(
         [BezierBuilder::new(true, (0.3, 0.))
             .extend([
-                BezierInst::line(0.6, 0.),
-                BezierInst::line(1., fcx.settings.width),
-                BezierInst::line(0.3, 0.),
+                line!(0.6, 0.),
+                line!(1., fcx.settings.width),
+                line!(0.3, 0.),
             ])
             .build()]
         .map(Arc::new),
---
+++ crates/monoxide-font/src/font/shape.rs
@@ -0,0 +1,33 @@
+use std::{ops::Range, sync::Arc};
+
+use monoxide_curves::point::Point2D;
+use monoxide_script::{ast::OutlineExpr, curl, dsl::SpiroBuilder, flat, g4};
+
+use super::math::mix;
+
+/// Renders a rectangle formed by drawing a line between points `start` and
+/// `end` and span it in the normal direction according to the given width.
+pub fn rect(start: impl Into<Point2D>, end: impl Into<Point2D>, width: f64) -> Arc<OutlineExpr> {
+    Arc::new(
+        SpiroBuilder::new(false)
+            .extend([flat!(start.into()), curl!(end.into())])
+            .build(),
+    )
+    .stroked(width)
+}
+
+/// Renders a ring delimited within the given x and y ranges.
+pub fn ring(xr: Range<f64>, yr: Range<f64>) -> Arc<OutlineExpr> {
+    let x0 = xr.start;
+    let x1 = xr.end;
+    let y0 = yr.start;
+    let y1 = yr.end;
+    let xm = mix(x0, x1, 0.5);
+    let ym = mix(y0, y1, 0.5);
+
+    Arc::new(
+        SpiroBuilder::new(true)
+            .extend([g4!(x0, ym), g4!(xm, y0), g4!(x1, ym), g4!(xm, y1)])
+            .build(),
+    )
+}
