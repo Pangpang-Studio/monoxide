1b2996527dc790939a651da33fb9d4fd8259d82a
rami3l <rami3l@outlook.com>
2025-06-16 13:06:36 +0800

    feat(font/glyph/c): test spiro rendering

--- crates/monoxide-font/src/font/glyph/c.rs
+++ crates/monoxide-font/src/font/glyph/c.rs
@@ -4,12 +4,21 @@ use monoxide_script::{
     line,
 };
 
+use crate::font::{
+    math::crange,
+    shape::{rect, ring},
+};
+
 pub fn c(fcx: &FontContext) -> SimpleGlyph {
-    SimpleGlyph::new([BezierBuilder::new(true, (0.3, 0.))
-        .extend([
-            line!(0.6, 0.),
-            line!(1., fcx.settings.width),
-            line!(0.3, 0.),
-        ])
-        .build()])
+    SimpleGlyph::new([
+        BezierBuilder::new(true, (0.6, 0.))
+            .extend([
+                line!(0.8, 0.),
+                line!(1., fcx.settings.width),
+                line!(0.6, 0.),
+            ])
+            .build(),
+        rect((0., 0.), (0.1, 0.4), 0.05),
+        ring(crange(0.4, 0.15), crange(0.2, 0.1)).stroked(0.1),
+    ])
 }
