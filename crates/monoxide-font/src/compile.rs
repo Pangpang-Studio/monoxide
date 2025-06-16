4135aeaf3ffc5c4eaf289f611e5206a24c74df90
Rynco Maekawa <lynzrand@outlook.com>
2025-06-16 22:46:27 +0800

    feat: Add main crate for rendering font to ttf

---
+++ crates/monoxide-font/src/compile.rs
@@ -0,0 +1,17 @@
+use monoxide_script::eval::AuxiliarySettings;
+
+mod font;
+
+fn main() {
+    let fcx = font::make_font().expect("Unable to create font");
+    let res = monoxide_script::eval::eval(
+        &fcx,
+        &AuxiliarySettings {
+            point_per_em: 2048,
+            font_name: "Monoxide".into(),
+            settings: fcx.settings.clone(),
+        },
+    );
+    res.write(std::fs::File::create("out.ttf").expect("Failed to open file"))
+        .expect("Failed to write font");
+}
