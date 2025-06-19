58ed382344543d203e2b14ca21518471b61370b8
rami3l <rami3l@outlook.com>
2025-06-19 13:36:10 +0800

    refactor: move `font/compile` example to `monoxide` binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use monoxide_font::make_font;
+use monoxide_script::eval::AuxiliarySettings;
+
 fn main() {
-    println!("Hello from Monoxide!");
+    eprintln!("Hello from Monoxide!");
+    let fcx = make_font().expect("Unable to create font");
+    let res = monoxide_script::eval::eval(
+        &fcx,
+        &AuxiliarySettings {
+            point_per_em: 2048,
+            font_name: "Monoxide".into(),
+        },
+    );
+    let fout = "out.ttf";
+    res.write(std::fs::File::create(fout).expect("Failed to open file"))
+        .expect("Failed to write font");
+    eprintln!("Successfully generated '{fout}'");
 }
