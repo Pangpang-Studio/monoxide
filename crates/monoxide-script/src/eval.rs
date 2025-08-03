6950f226f732909fd35ac932a8468c501cee8487
Rynco Maekawa <lynzrand@outlook.com>
2025-08-03 16:29:42 +0800

    feat: Add more data to font creation

--- crates/monoxide-script/src/eval.rs
+++ crates/monoxide-script/src/eval.rs
@@ -4,7 +4,8 @@ use monoxide_curves::{point::Point2D, xform::Affine2D};
 use monoxide_ttf::{
     hl,
     model::{
-        FontFile, Outline, TrueTypeTables, cmap, glyf, head, hhea, hmtx, name, os2, post, ufword,
+        FontFile, Outline, TrueTypeTables, cmap, fword, glyf, head, hhea, hmtx, name, os2, post,
+        ufword,
     },
 };
 use petgraph::prelude::DiGraphMap;
@@ -80,6 +81,7 @@ fn create_tables(
     let glyf = glyf::Table { glyphs };
     let loca = hl::loca::glyf_to_loca(&glyf);
     let maxp = hl::maxp::glyf_to_maxp(&glyf);
+    let (x_min, y_min, x_max, y_max) = xy_minmax(&glyf);
 
     let mappings = scx
         .cmap
@@ -98,14 +100,17 @@ fn create_tables(
 
     let outline = Outline::TrueType(TrueTypeTables { glyf, loca, maxp });
 
+    // hmtx for monospaced font
+    let width_in_font_units = aux.point_per_em as f64 * cx.settings.width;
     let hmtx = hmtx::Table {
         metrics: vec![hmtx::LongHorizontalMetric {
-            advance_width: (aux.point_per_em as f64 * cx.settings.width) as ufword,
+            advance_width: width_in_font_units as ufword,
             left_side_bearing: 0,
         }],
         left_side_bearings: vec![0; scx.glyph_list.len() - 1],
     };
 
+    // Calculate other tables
     let head = head::Table {
         font_revision: 0,
         checksum_adjustment: 0,
@@ -113,22 +118,22 @@ fn create_tables(
         units_per_em: aux.point_per_em,
         created: SystemTime::now(),
         modified: SystemTime::now(),
-        x_min: 0,
-        y_min: 0,
-        x_max: 1024,
-        y_max: 1024,
+        x_min,
+        y_min,
+        x_max,
+        y_max,
         mac_style: head::MacStyle::REGULAR,
         lowest_rec_ppem: 72,
     };
 
     let hhea = hhea::Table {
-        ascender: 1024,
-        descender: -256,
+        ascender: (aux.point_per_em as f64 * cx.settings.cap_height) as fword,
+        descender: (aux.point_per_em as f64 * cx.settings.descender) as fword,
         line_gap: 0,
-        advance_width_max: 1024,
+        advance_width_max: width_in_font_units as ufword,
         min_left_side_bearing: 0,
         min_right_side_bearing: 0,
-        x_max_extent: 1024,
+        x_max_extent: (width_in_font_units * 2.) as fword,
         caret_slope_rise: 1,
         caret_slope_run: 0,
         caret_offset: 0,
@@ -148,22 +153,29 @@ fn create_tables(
         .collect(),
     };
 
+    let subscript_sz = (aux.point_per_em as f64 * cx.settings.x_height) as fword;
+    let subscript_offset = (aux.point_per_em as f64 * cx.settings.x_height / 4.) as fword;
+    let strikeout_sz = (0.05 * aux.point_per_em as f64) as fword;
+    let strikeout_pos = (aux.point_per_em as f64 * cx.settings.x_height / 2.) as fword;
+    let ascender = (aux.point_per_em as f64 * cx.settings.cap_height) as fword;
+    let descender = (aux.point_per_em as f64 * cx.settings.descender) as fword;
+    let x_height = (aux.point_per_em as f64 * cx.settings.x_height) as fword;
     let os2 = os2::Table {
-        x_avg_char_width: 1024,
-        us_weight_class: 400,
+        x_avg_char_width: width_in_font_units as fword,
+        us_weight_class: 400, // TODO: set weight
         us_width_class: 5,
         usage_permission: os2::UsagePermissionKind::EditableEmbedding,
         fs_type: os2::FsTypeUpper::Nothing,
-        y_subscript_x_size: 512,
-        y_subscript_y_size: 512,
+        y_subscript_x_size: subscript_sz,
+        y_subscript_y_size: subscript_sz,
         y_subscript_x_offset: 0,
-        y_subscript_y_offset: 128,
-        y_superscript_x_size: 512,
-        y_superscript_y_size: 512,
+        y_subscript_y_offset: subscript_offset,
+        y_superscript_x_size: subscript_sz,
+        y_superscript_y_size: subscript_sz,
         y_superscript_x_offset: 0,
-        y_superscript_y_offset: 768,
-        y_strikeout_size: 50,
-        y_strikeout_position: 258,
+        y_superscript_y_offset: aux.point_per_em as fword - subscript_offset,
+        y_strikeout_size: strikeout_sz,
+        y_strikeout_position: strikeout_pos,
         s_family_class: os2::SFamilyClass::NoClassification,
         panose_classification: os2::PanroseClassification {
             family_type: 2,
@@ -182,14 +194,14 @@ fn create_tables(
         fs_selection: os2::FsSelectionKind::Regular,
         first_char_index: 0,
         last_char_index: 0xffff,
-        s_typo_ascender: 1024,
-        s_typo_descender: -256,
+        s_typo_ascender: ascender,
+        s_typo_descender: descender,
         s_typo_line_gap: 0,
-        us_win_ascent: 1024,
-        us_win_descent: 256,
+        us_win_ascent: ascender as u16,
+        us_win_descent: (-descender) as u16,
         code_page_range: os2::CodePageRange::Latin1,
-        sx_height: 768,
-        s_cap_height: 1024,
+        sx_height: x_height,
+        s_cap_height: ascender,
         us_default_char: 65,
         us_break_char: 65,
         us_max_context: 0,
@@ -217,4 +229,22 @@ fn create_tables(
         outline,
         dsig: Some(Default::default()),
     }
+}
+
+/// Calculate `(xmin, ymin, xmax, ymax)` from glyf table
+fn xy_minmax(glyf: &glyf::Table) -> (fword, fword, fword, fword) {
+    let mut xmin = 0;
+    let mut ymin = 0;
+    let mut xmax = 0;
+    let mut ymax = 0;
+
+    for it in &glyf.glyphs {
+        let common = it.common();
+        xmin = xmin.min(common.x_min);
+        ymin = ymin.min(common.y_min);
+        xmax = xmax.max(common.x_max);
+        ymax = ymax.max(common.y_max);
+    }
+
+    (xmin, ymin, xmax, ymax)
 }
