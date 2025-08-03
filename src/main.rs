61e364c9320c1dc6cce5e060614fced10de5dd3e
Rynco Maekawa <lynzrand@outlook.com>
2025-08-03 15:44:21 +0800

    feat: Add logging to standalone build

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use monoxide_font::make_font;
 use monoxide_script::eval::AuxiliarySettings;
 
 fn main() {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::from_default_env()
+                .add_directive(tracing_subscriber::filter::LevelFilter::INFO.into()),
+        )
+        .init();
     eprintln!("Hello from Monoxide!");
     let fcx = make_font().expect("Unable to create font");
     let res = monoxide_script::eval::eval(
