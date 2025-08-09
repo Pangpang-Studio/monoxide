4595ada6d6dd84cc4d12760f1931289cb29aab22
rami3l <rami3l@outlook.com>
2025-08-09 20:54:15 +0800

    refactor(font/glyph/i): extract `LShape`

--- crates/monoxide-font/src/font/glyph/i.rs
+++ crates/monoxide-font/src/font/glyph/i.rs
@@ -1,32 +1,76 @@
+use std::{ops::Range, sync::Arc};
+
 use monoxide_script::{
-    ast::Glyph,
+    ast::{Glyph, OutlineExpr},
     corner,
-    dsl::{IntoOutlineExt, SpiroBuilder},
+    dsl::{IntoOutline, IntoOutlines, IntoOutlinesExt, SpiroBuilder},
     g4, let_settings,
 };
 
 use super::InputContext;
-use crate::font::{
-    math::mix,
-    shape::{Rect, Ring},
-};
+use crate::font::{math::mix, shape::Ring};
 
 pub fn i(fcx: &InputContext) -> Glyph {
-    let_settings! { { cap, dtr, mid, sbl, sbr, stw, xh } = fcx.settings(); }
+    let_settings! { { sbl, sbr, stw, xh } = fcx.settings(); }
 
     let hstw = stw / 2.;
 
     Glyph::builder()
-        .outline(
+        .outlines(LShape::new(sbl..sbr, hstw..(xh - hstw), 0.85).stroked(stw))
+        .outline(i_dot(fcx))
+        .build()
+}
+
+pub fn i_dot(fcx: &InputContext) -> impl IntoOutline {
+    let_settings! { { cap, dtr, mid, sbl } = fcx.settings(); }
+    Ring::at((mix(mid, sbl, 0.95), cap), (dtr, dtr))
+}
+
+pub struct LShape {
+    pub x_range: Range<f64>,
+    pub y_range: Range<f64>,
+    pub top_bar_scale: f64,
+}
+
+impl LShape {
+    pub fn new(x_range: Range<f64>, y_range: Range<f64>, top_bar_scale: f64) -> Self {
+        Self {
+            x_range,
+            y_range,
+            top_bar_scale,
+        }
+    }
+}
+
+impl IntoOutlines for LShape {
+    fn into_outlines(self) -> impl Iterator<Item = Arc<OutlineExpr>> {
+        let Self {
+            x_range: Range {
+                start: x_min,
+                end: x_max,
+            },
+            y_range: Range {
+                start: y_min,
+                end: y_max,
+            },
+            top_bar_scale,
+        } = self;
+
+        let mid = (x_min + x_max) / 2.;
+        let hw = (x_max - x_min) / 2.;
+
+        vec![
             SpiroBuilder::open()
                 .insts([
-                    g4!(mid, stw),
-                    corner!(mid, xh - hstw),
-                    g4!(mix(mid, sbl, 0.175), xh - hstw),
+                    g4!(mid, y_min),
+                    corner!(mid, y_max),
+                    g4!(mid - top_bar_scale * hw, y_max),
                 ])
-                .stroked(stw),
-        )
-        .outline(Rect::new((sbl, hstw), (sbr, hstw), stw))
-        .outline(Ring::at((mix(mid, sbl, 0.95), cap), (dtr, dtr)))
-        .build()
+                .into_outline(),
+            SpiroBuilder::open()
+                .insts([g4!(mid - hw, y_min), g4!(mid + hw, y_min)])
+                .into_outline(),
+        ]
+        .into_iter()
+    }
 }
