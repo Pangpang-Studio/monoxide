7478dad990aa001d848583558ef05e50a3074e7b
rami3l <rami3l@outlook.com>
2025-10-05 20:49:26 +0800

    feat(font/glyph/a): simplify shape of `A`

--- crates/monoxide-font/src/font/glyph/a.rs
+++ crates/monoxide-font/src/font/glyph/a.rs
@@ -11,27 +11,18 @@ pub fn a_cap(cx: &InputContext) -> Glyph {
         {
             lower_left,
             lower_right,
-            upper_left,
-            upper_right,
             upper_mid,
             lower_mid,
             stw,
         } = cx.settings();
     }
 
-    let curl_height = 0.65;
-    let curl_height1 = 0.6;
     let bar_height = 0.65;
 
-    let lm1 = mix(lower_left, upper_left, curl_height);
-    let lm1o = mix(lower_left, upper_left, curl_height1);
-    let lm2 = mix(lm1o, upper_mid, 0.5);
     let left = SpiroBuilder::open()
         .insts([
-            corner!(lower_left),
-            curl!(lm1).align(Alignment::Left),
-            flat!(lm2).align(Alignment::Middle),
-            corner!(upper_mid).heading(Dir::U).align(Alignment::Middle),
+            g4!(lower_left).heading(Dir::D),
+            g4!(upper_mid).heading(Dir::U).align(Alignment::Middle),
         ])
         .stroked(stw);
 
@@ -41,8 +32,8 @@ pub fn a_cap(cx: &InputContext) -> Glyph {
 
     let bar = SpiroBuilder::open()
         .insts([
-            corner!(mix(lower_left, upper_left, bar_height)),
-            corner!(mix(lower_right, upper_right, bar_height)),
+            corner!(mix(lower_left, upper_mid, bar_height)),
+            corner!(mix(lower_right, upper_mid, bar_height)),
         ])
         .stroked(stw);
 
