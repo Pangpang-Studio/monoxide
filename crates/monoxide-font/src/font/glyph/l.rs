8898c9da199f233837aa13fb068a9f013abb1a30
rami3l <rami3l@outlook.com>
2025-10-06 20:40:48 +0800

    chore(font/glyph/l): remove unused imports

--- crates/monoxide-font/src/font/glyph/l.rs
+++ crates/monoxide-font/src/font/glyph/l.rs
@@ -3,7 +3,7 @@ use std::{ops::Range, sync::Arc};
 use monoxide_script::prelude::*;
 
 use super::InputContext;
-use crate::font::{dir::Alignment, shape::Rect};
+use crate::font::dir::Alignment;
 
 pub fn l(cx: &InputContext) -> Glyph {
     let_settings! { { sbl, sbr, stw, cap } = cx.settings(); }
