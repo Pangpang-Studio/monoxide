6ac7e57ad151c2c931b4b0f9194ba5f40ff2a7c3
rami3l <rami3l@outlook.com>
2025-10-06 11:24:57 +0800

    refactor(font/glyph/p): simplify transformation

--- crates/monoxide-font/src/font/glyph/p.rs
+++ crates/monoxide-font/src/font/glyph/p.rs
@@ -3,15 +3,13 @@ use monoxide_script::prelude::*;
 use super::{InputContext, d::DShape};
 
 pub fn p(cx: &InputContext) -> Glyph {
-    let_settings! { { xh, mih, dsc, lower_mid } = cx.settings; }
+    let_settings! { { xh, mid, mih, dsc } = cx.settings; }
     Glyph::builder()
         .outlines(
-            DShape::with_height(&cx.settings, xh - dsc)
-                .transformed(Affine2D::mirrored_along(lower_mid, Point2D::unit_y()))
-                .transformed(Affine2D::mirrored_along(
-                    (0., mih).into(),
-                    Point2D::unit_x(),
-                )),
+            DShape::with_height(&cx.settings, xh - dsc).transformed(
+                Affine2D::mirrored_along((mid, 0.).into(), Point2D::unit_y())
+                    .mirror_along((0., mih).into(), Point2D::unit_x()),
+            ),
         )
         .build()
 }
