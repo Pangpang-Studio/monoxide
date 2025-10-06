533d375ab89f6ec489360e7859209e2a3e00888f
rami3l <rami3l@outlook.com>
2025-10-06 22:46:34 +0800

    fix(font/glyph/d): adjust `Bowl`'s stroke width

--- crates/monoxide-font/src/font/glyph/d.rs
+++ crates/monoxide-font/src/font/glyph/d.rs
@@ -20,8 +20,7 @@ impl DShape {
     pub fn from_settings(settings: &FontParamSettings) -> Self {
         let_settings! { { cap, mid, mih, ovs, sbl, sbr, stw } = settings; }
 
-        // TODO: Stroke should narrow at lower join of the bowl.
-        let bowl = OShape::new((mid, mih), (mid - sbl, mih), ovs)
+        let bowl = Bowl::new((mid, mih), (mid - sbl, mih), ovs)
             .stroked(stw)
             .into_outline();
         let pipe = Rect::new((sbr, 0.), (sbr, cap), stw).aligned(Alignment::Right);
@@ -38,5 +37,66 @@ impl DShape {
 impl IntoOutlines for DShape {
     fn into_outlines(self) -> impl Iterator<Item = std::sync::Arc<OutlineExpr>> {
         [self.bowl, self.pipe.into_outline()].into_iter()
+    }
+}
+
+struct Bowl {
+    pub o_shape: OShape,
+}
+
+impl Bowl {
+    pub fn new(center: impl Into<Point2D>, radii: impl Into<Point2D>, ovs: f64) -> Self {
+        Self {
+            o_shape: OShape::new(center, radii, ovs),
+        }
+    }
+
+    pub fn mid_curve_h(&self) -> f64 {
+        self.o_shape.mid_curve_h()
+    }
+
+    pub fn mid_curve_w(&self) -> f64 {
+        self.o_shape.mid_curve_w()
+    }
+
+    pub fn end_curve_h(&self) -> f64 {
+        self.o_shape.end_curve_h()
+    }
+}
+
+impl IntoOutline for Bowl {
+    fn into_outline(self) -> std::sync::Arc<OutlineExpr> {
+        let OShape {
+            center: Point2D { x, y },
+            radii: Point2D { x: rx, y: ry },
+            ovs,
+        } = self.o_shape;
+
+        let mid_curve_w = self.mid_curve_w();
+        let mid_curve_h = self.mid_curve_h();
+        let end_curve_h = self.end_curve_h();
+
+        let y_hi = y + ry;
+        let y_lo = y - ry;
+
+        SpiroBuilder::closed()
+            .insts([
+                // Left side
+                flat!(x - rx, y_hi - end_curve_h),
+                curl!(x - rx, y_lo + end_curve_h),
+                // Bottom arc
+                g4!(x - mid_curve_w, y_lo + mid_curve_h)
+                    .aligned(Alignment::Right)
+                    .width(1.),
+                g4!(x, y_lo - ovs),
+                g4!(x + mid_curve_w, y_lo + mid_curve_h * 1.25).width(0.9),
+                // Right side
+                g4!(x + rx, y_hi - end_curve_h).width(0.9),
+                // Top arc
+                g4!(x + mid_curve_w, y_hi - mid_curve_h).width(1.),
+                g4!(x, y_hi + ovs),
+                g4!(x - mid_curve_w, y_hi - mid_curve_h),
+            ])
+            .into_outline()
     }
 }
