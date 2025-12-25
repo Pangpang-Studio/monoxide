19a6c9df4188f3ee95afa8fb8acb1001fa05ff33
rami3l <rami3l@outlook.com>
2025-12-25 22:19:40 +0100

    fix(curves/stroke): refine `stroke_spiro()` preconditions

--- crates/monoxide-curves/src/error.rs
+++ crates/monoxide-curves/src/error.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 #[derive(Clone, Debug, thiserror::Error)]
 pub enum Error {
     #[error("spiro curve could not be converted to cubic bezier curves")]
@@ -5,6 +7,15 @@ pub enum Error {
 
     #[error("spiro curve is not a single piece")]
     SpiroBroken,
+
+    #[error("internal error: {0}")]
+    Internal(Cow<'static, str>),
+}
+
+impl Error {
+    pub fn internal<S: Into<Cow<'static, str>>>(msg: S) -> Self {
+        Error::Internal(msg.into())
+    }
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
