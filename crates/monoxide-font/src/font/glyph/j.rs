840cdf74fd3c7ea2f9e542e4024ddb875da3a69a
rami3l <rami3l@outlook.com>
2026-02-13 18:42:10 +0100

    fix(font/glyph/j): adjust position of `J`

--- crates/monoxide-font/src/font/glyph/j.rs
+++ crates/monoxide-font/src/font/glyph/j.rs
@@ -3,7 +3,12 @@ use std::sync::Arc;
 use monoxide_script::prelude::*;
 
 use super::InputContext;
-use crate::font::{dir::Alignment, glyph::n::Hook, settings::FontParamSettings, shape::Rect};
+use crate::font::{
+    dir::{Alignment, Dir},
+    glyph::n::Hook,
+    settings::FontParamSettings,
+    shape::Rect,
+};
 
 pub fn j_cap(cx: &InputContext) -> Glyph {
     Glyph::builder()
@@ -14,27 +19,35 @@ pub fn j_cap(cx: &InputContext) -> Glyph {
 pub struct JCapShape {
     pub hook: Arc<OutlineExpr>,
     pub pipe: Rect,
+    pub offset: Point2D,
 }
 
 impl JCapShape {
     pub fn from_settings(settings: &FontParamSettings) -> Self {
         let_settings! { { cap, mid, mih, ovs, sbl, sbr, stw, xh } = settings; }
 
-        let hook = Hook::new((mid, mih + cap - xh), (mid - sbl, mih), ovs)
-            .with_hook_tip_heading((3., 1.))
+        let hook = Hook::new((mid, mih + cap - xh), (1.05 * (mid - sbl), mih), ovs)
+            .with_hook_tip_heading(Dir::L)
             .stroked(1.1 * stw)
             .transformed(Affine2D::mirrored_along(
                 (0., cap / 2.).into(),
                 Point2D::unit_x(),
             ));
+
         let pipe = Rect::new((sbl.midpoint(mid), cap), (sbr, cap), stw).aligned(Alignment::Left);
 
-        Self { hook, pipe }
+        Self {
+            hook,
+            pipe,
+            offset: (-stw / 2., 0.).into(),
+        }
     }
 }
 
 impl IntoOutlines for JCapShape {
     fn into_outlines(self) -> impl Iterator<Item = std::sync::Arc<OutlineExpr>> {
-        [self.hook.into_outline(), self.pipe.into_outline()].into_iter()
+        [self.hook.into_outline(), self.pipe.into_outline()]
+            .into_iter()
+            .map(move |it| it.transformed(Affine2D::translated(self.offset)))
     }
 }
