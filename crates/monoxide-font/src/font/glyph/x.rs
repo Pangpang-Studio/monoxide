d464b8630216c1d125664202d7d7e8deba7446b2
rami3l <rami3l@outlook.com>
2026-02-15 14:08:36 +0100

    fix(font/glyph/x): improve glyph and stroke widths

--- crates/monoxide-font/src/font/glyph/x.rs
+++ crates/monoxide-font/src/font/glyph/x.rs
@@ -6,21 +6,22 @@ use crate::font::dir::Dir;
 pub fn x(cx: &InputContext) -> Glyph {
     let_settings! { { sbl, sbr, stw, xh } = cx.settings(); }
 
-    let slash = SpiroBuilder::open()
-        .insts([
-            g4!(sbl, 0.).heading(Dir::D).aligned(0.25),
-            g4!(sbr, xh).heading(Dir::U).aligned(0.75),
-        ])
-        .into_outline();
-    let backslash = SpiroBuilder::open()
-        .insts([
-            g4!(sbr, 0.).heading(Dir::D).aligned(0.75),
-            g4!(sbl, xh).heading(Dir::U).aligned(0.25),
-        ])
-        .into_outline();
+    let aln = 0.2;
+    let slash = SpiroBuilder::open().insts([
+        g4!(sbl, 0.).heading(Dir::D).aligned(aln),
+        g4!(sbr, xh).heading(Dir::U).aligned(1. - aln),
+    ]);
+
+    let backslash = SpiroBuilder::open().insts([
+        g4!(sbr, 0.).heading(Dir::D).aligned(1. - aln),
+        g4!(sbl, xh).heading(Dir::U).aligned(aln),
+    ]);
 
     Glyph::builder()
-        // TODO: Find out how the scaling factor is determined.
-        .outlines([slash, backslash].stroked(stw * 0.9))
+        .outlines(
+            [slash, backslash]
+                .into_iter()
+                .map(move |it| it.into_outline().stroked(stw)),
+        )
         .build()
 }
