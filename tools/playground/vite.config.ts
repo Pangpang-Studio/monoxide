6d2828ab356c2e6d9018a78abf5ae04179bf69cc
rami3l <rami3l@outlook.com>
2026-03-08 11:42:59 +0100

    fix(playground): support base URL override

--- tools/playground/vite.config.ts
+++ tools/playground/vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '')
+
+  return {
+    base: env.VITE_BASE_URL || '/',
+    plugins: [vue(), tailwindcss()],
+  }
 })
