396a873d38512ab769072793a21d52c5a7e791c4
rami3l <rami3l@outlook.com>
2026-04-19 22:22:43 +0200

    style: address clippy warnings

--- crates/monoxide-curves/src/stroke/mod.rs
+++ crates/monoxide-curves/src/stroke/mod.rs
@@ -217,10 +217,10 @@ fn stroke_spiro_raw(
 
     // Determine the stroke attributes (width factors, alignments, etc.) at each
     // control point.
-    let stroke_attrs = solve_stroke_attrs(curve, is_closed, &cubic, &indices)?;
+    let stroke_attrs = solve_stroke_attrs(curve, is_closed, cubic, &indices)?;
 
     // Calculate tangent for each control point.
-    let actual_tangents = tangents::calc_tangents(&curve.points, &cubic, &indices);
+    let actual_tangents = tangents::calc_tangents(&curve.points, cubic, &indices);
 
     // the curve on the left side of the stroke
     let mut left_curve = Vec::new();
