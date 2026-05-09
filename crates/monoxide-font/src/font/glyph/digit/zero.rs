037e09a060c2d4296888e67c1292c7654b484ae1
rami3l <rami3l@outlook.com>
2026-05-09 11:42:59 +0200

    refactor(font/glyph/o): distinct `OCapShape` from `OShape`

--- crates/monoxide-font/src/font/glyph/digit/zero.rs
+++ crates/monoxide-font/src/font/glyph/digit/zero.rs
@@ -4,32 +4,35 @@ use crate::{
     InputContext,
     font::{
         dir::{Alignment, Dir},
-        glyph::o::OShape,
+        glyph::o::{OCapShape, OShape},
     },
 };
 
 pub fn zero(cx: &InputContext) -> Glyph {
     let_settings! { { mid, cap, ovs, ovh, sbl, stw } = cx.settings(); }
 
-    let o_shape @ OShape {
-        center: Point2D { y, .. },
-        radii: Point2D { y: ry, .. },
-        ..
-    } = OShape::new((mid, cap / 2.), (mid - sbl, cap / 2.), ovs).with_ovh(ovh);
+    let o_cap_shape @ OCapShape {
+        o_shape:
+            OShape {
+                center: Point2D { y, .. },
+                radii: Point2D { y: ry, .. },
+                ..
+            },
+    } = OCapShape::new((mid, cap / 2.), (mid - sbl, cap / 2.), ovs).with_ovh(ovh);
 
-    let end_curve_h = o_shape.end_curve_h();
+    let end_curve_h = o_cap_shape.end_curve_h();
 
     let slash = SpiroBuilder::open().insts([
-        g4!(o_shape.left() + stw, y - ry + end_curve_h)
+        g4!(o_cap_shape.left() + stw, y - ry + end_curve_h)
             .heading(Dir::L)
             .aligned(Alignment::Left),
-        g4!(o_shape.right() - stw, y + ry - end_curve_h)
+        g4!(o_cap_shape.right() - stw, y + ry - end_curve_h)
             .heading(Dir::R)
             .aligned(Alignment::Right),
     ]);
 
     Glyph::builder()
-        .outline(o_shape.stroked(stw))
+        .outline(o_cap_shape.stroked(stw))
         .outline(slash.stroked(stw))
         .build()
 }
