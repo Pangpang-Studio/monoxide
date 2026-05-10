aadc062d9194c657e3f720a43c1716441aa2ce1f
rami3l <rami3l@outlook.com>
2026-05-10 22:41:02 +0200

    refactor(font/glyph/v): rewrite with `SlashShape`

--- crates/monoxide-font/src/font/glyph/v.rs
+++ crates/monoxide-font/src/font/glyph/v.rs
@@ -2,28 +2,23 @@ use monoxide_script::prelude::*;
 
 use crate::{
     InputContext,
-    font::dir::{Alignment, Dir},
+    font::glyph::sym::{SlashAlignment, SlashShape},
 };
 
 pub fn v(cx: &InputContext) -> Glyph {
     let_settings! { { sbl, mid, sbr, stw, xh } = cx.settings(); }
 
     let aln = 0.2;
-    let slash = SpiroBuilder::open().insts([
-        g4!(mid, 0.).heading(Dir::D).aligned(Alignment::Middle),
-        g4!(sbr, xh).heading(Dir::U).aligned(1. - aln),
-    ]);
+    let slash = SlashShape::new(mid..sbr, 0.0..xh).with_aln(SlashAlignment::new(0.5, 1. - aln));
 
-    let backslash = SpiroBuilder::open().insts([
-        g4!(mid, 0.).heading(Dir::D).aligned(Alignment::Middle),
-        g4!(sbl, xh).heading(Dir::U).aligned(aln),
-    ]);
+    let backslash = SlashShape {
+        x_range: sbl..mid,
+        ..slash.clone()
+    }
+    .back();
 
     Glyph::builder()
-        .outlines(
-            [slash, backslash]
-                .into_iter()
-                .map(move |it| it.into_outline().stroked(stw)),
-        )
+        .outlines(slash.stroked(stw))
+        .outlines(backslash.stroked(stw))
         .build()
 }
