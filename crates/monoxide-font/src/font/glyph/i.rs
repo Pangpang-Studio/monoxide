706ca50d21fbeb41de79dc756c0da49896d1efa4
rami3l <rami3l@outlook.com>
2026-05-11 21:22:00 +0200

    feat(font/glyph/i): add `i_cap()`

--- crates/monoxide-font/src/font/glyph/i.rs
+++ crates/monoxide-font/src/font/glyph/i.rs
@@ -15,6 +15,18 @@ pub fn i(cx: &InputContext) -> Glyph {
         .build()
 }
 
+pub fn i_cap(cx: &InputContext) -> Glyph {
+    let_settings! { { sbl, sbr, stw, cap } = cx.settings(); }
+
+    Glyph::builder()
+        .outlines(
+            LShape::new(sbl..sbr, 0.0..cap)
+                .with_top_bar_scale((-1.)..1.)
+                .stroked(stw),
+        )
+        .build()
+}
+
 pub fn dot(settings: &FontParamSettings) -> Ring {
     let_settings! { { cap, dtr, mid, sbl } = settings; }
     Ring::at((mix(mid, sbl, 0.97), 0.97 * cap), (dtr, dtr))
