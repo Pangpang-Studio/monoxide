950077246c2b807ba20ad50942d9521bff31bbfc
rami3l <rami3l@outlook.com>
2026-05-11 20:45:08 +0200

    refactor(font/glyph/z): rewrite with `SlashShape`

--- crates/monoxide-font/src/font/glyph/z.rs
+++ crates/monoxide-font/src/font/glyph/z.rs
@@ -2,11 +2,7 @@ use monoxide_script::prelude::*;
 
 use crate::{
     InputContext,
-    font::{
-        dir::{Alignment, Dir},
-        glyph::c::CShape,
-        shape::Rect,
-    },
+    font::{dir::Alignment, glyph::c::CShape, glyph::sym::SlashShape, shape::Rect},
 };
 
 pub fn z(cx: &InputContext) -> Glyph {
@@ -15,25 +11,13 @@ pub fn z(cx: &InputContext) -> Glyph {
     let serif_l = CShape::new((mid, mih), (mid - sbl, mih), ovs).aperture_curve_h_lo();
     let serif = Rect::new((sbl, xh - serif_l - stw), (sbl, xh)).aligned(Alignment::Left);
 
-    let aln = 0.;
-    let slash = SpiroBuilder::open().insts([
-        g4!(sbl, stw).heading(Dir::D).aligned(aln).width(0.9),
-        g4!(sbr, xh - stw).heading(Dir::U).aligned(1. - aln),
-    ]);
+    let slash = SlashShape::new(sbl..sbr, stw..(xh - stw));
 
     let top_bar = Rect::new((sbl, xh), (sbr, xh)).aligned(Alignment::Left);
     let bottom_bar = Rect::new((sbl, 0.), (sbr, 0.)).aligned(Alignment::Right);
 
     Glyph::builder()
-        .outlines(
-            [
-                serif.into_outline(),
-                top_bar.into_outline(),
-                slash.into_outline(),
-                bottom_bar.into_outline(),
-            ]
-            .into_iter()
-            .map(move |it| it.stroked(stw)),
-        )
+        .outlines([serif, top_bar, bottom_bar].map(move |it| it.into_outline().stroked(stw)))
+        .outlines(slash.stroked(0.9 * stw))
         .build()
 }
