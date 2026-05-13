4e8462f65cf262d90f6e8ff690c178531e64eb91
rami3l <rami3l@outlook.com>
2026-05-13 23:39:41 +0200

    feat(font/glyph/h): add `h_cap()`

--- crates/monoxide-font/src/font/glyph/h.rs
+++ crates/monoxide-font/src/font/glyph/h.rs
@@ -1,11 +1,26 @@
 use monoxide_script::prelude::*;
 
-use crate::{InputContext, font::glyph::n::NShape};
+use crate::{
+    InputContext,
+    font::{dir::Alignment, glyph::n::NShape, shape::Rect},
+};
 
 pub fn h(cx: &InputContext) -> Glyph {
     let_settings! { { cap } = cx.settings(); }
 
     Glyph::builder()
         .outlines(NShape::from_settings(cx.settings()).with_pipe_height(cap))
+        .build()
+}
+
+pub fn h_cap(cx: &InputContext) -> Glyph {
+    let_settings! { { lower_left, lower_right, upper_left, upper_right, stw, cap } = cx.settings(); }
+
+    let left = Rect::new(lower_left, upper_left).aligned(Alignment::Left);
+    let right = Rect::new(lower_right, upper_right).aligned(Alignment::Right);
+    let bar = Rect::new((lower_left.x, cap / 2.), (lower_right.x, cap / 2.));
+
+    Glyph::builder()
+        .outlines([left, right, bar].map(|rect| rect.stroked(stw).into_outline()))
         .build()
 }
