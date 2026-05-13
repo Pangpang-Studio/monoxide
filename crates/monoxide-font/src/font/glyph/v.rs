67b6c54c5a3a686a3afe6dd18cc997f30f8f6dde
rami3l <rami3l@outlook.com>
2026-05-14 00:11:23 +0200

    refactor(font/glyph/v): extract `VShape`

--- crates/monoxide-font/src/font/glyph/v.rs
+++ crates/monoxide-font/src/font/glyph/v.rs
@@ -1,3 +1,5 @@
+use std::{ops::Range, sync::Arc};
+
 use monoxide_script::prelude::*;
 
 use crate::{
@@ -6,19 +8,40 @@ use crate::{
 };
 
 pub fn v(cx: &InputContext) -> Glyph {
-    let_settings! { { sbl, mid, sbr, stw, xh } = cx.settings(); }
-
-    let aln = 0.2;
-    let slash = Slash::new(mid..sbr, 0.0..xh).with_aln(SlashAlignment::new(0.5, 1. - aln));
-
-    let backslash = Slash {
-        xr: sbl..mid,
-        ..slash.clone()
-    }
-    .back();
+    let_settings! { { sbl, sbr, stw, xh } = cx.settings(); }
 
     Glyph::builder()
-        .outline(slash.stroked(stw))
-        .outline(backslash.stroked(stw))
+        .outlines(VShape::new(sbl..sbr, 0.0..xh).stroked(stw))
         .build()
+}
+
+struct VShape {
+    xr: Range<f64>,
+    yr: Range<f64>,
+}
+
+impl VShape {
+    fn new(xr: Range<f64>, yr: Range<f64>) -> Self {
+        Self { xr, yr }
+    }
+}
+
+impl IntoOutlines for VShape {
+    type Outlines = [Arc<OutlineExpr>; 2];
+
+    fn into_outlines(self) -> Self::Outlines {
+        let Self { xr, yr } = self;
+        let mid = xr.start.midpoint(xr.end);
+
+        let aln = 0.2;
+        let slash =
+            Slash::new(mid..xr.end, yr.clone()).with_aln(SlashAlignment::new(0.5, 1. - aln));
+        let backslash = Slash {
+            xr: xr.start..mid,
+            ..slash.clone()
+        }
+        .back();
+
+        [slash.into_outline(), backslash.into_outline()]
+    }
 }
