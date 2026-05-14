0c993f1c3618a4955c9c5c5ada7374e7274fbe2a
rami3l <rami3l@outlook.com>
2026-05-14 12:11:29 +0200

    feat(font/glyph/q): add `q_cap()`

--- crates/monoxide-font/src/font/glyph/q.rs
+++ crates/monoxide-font/src/font/glyph/q.rs
@@ -1,6 +1,7 @@
 use monoxide_script::prelude::*;
 
-use super::{InputContext, d::DShape};
+use super::{InputContext, d::DShape, o::OCapShape};
+use crate::font::shape::{Slash, SlashAlignment};
 
 pub fn q(cx: &InputContext) -> Glyph {
     let_settings! { { xh, mih, dsc } = cx.settings; }
@@ -10,5 +11,19 @@ pub fn q(cx: &InputContext) -> Glyph {
                 .with_height(xh - dsc)
                 .transformed(Affine2D::mirrored_along((0., mih), (1., 0.))),
         )
+        .build()
+}
+
+pub fn q_cap(cx: &InputContext) -> Glyph {
+    let_settings! { { mid, cap, ovs, ovh, sbl, sbr, stw, dsc } = cx.settings(); }
+
+    let o_cap = OCapShape::new((mid, cap / 2.), (mid - sbl, cap / 2.), ovs).with_ovh(ovh);
+    let tail = Slash::new(mid..mid.midpoint(sbr), (dsc * 0.75)..0.)
+        .with_aln(SlashAlignment::symm(0.5))
+        .back();
+
+    Glyph::builder()
+        .outline(o_cap.stroked(stw))
+        .outline(tail.stroked(stw))
         .build()
 }
