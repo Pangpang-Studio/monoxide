8ceea705e8fe65a8309b4232d10f93d06e6fb066
rami3l <rami3l@outlook.com>
2026-05-14 10:23:23 +0200

    feat(font/glyph/x): add `x_cap()`

--- crates/monoxide-font/src/font/glyph/x.rs
+++ crates/monoxide-font/src/font/glyph/x.rs
@@ -1,3 +1,5 @@
+use std::{ops::Range, sync::Arc};
+
 use monoxide_script::prelude::*;
 
 use crate::{
@@ -8,11 +10,37 @@ use crate::{
 pub fn x(cx: &InputContext) -> Glyph {
     let_settings! { { sbl, sbr, stw, xh } = cx.settings(); }
 
-    let slash = Slash::new(sbl..sbr, (0.)..xh).with_aln(SlashAlignment::symm(0.2));
-    let backslash = slash.clone().back();
+    Glyph::builder()
+        .outlines(XShape::new(sbl..sbr, 0.0..xh).stroked(stw))
+        .build()
+}
+
+pub fn x_cap(cx: &InputContext) -> Glyph {
+    let_settings! { { sbl, sbr, stw, cap } = cx.settings(); }
 
     Glyph::builder()
-        .outline(slash.stroked(stw))
-        .outline(backslash.stroked(stw))
+        .outlines(XShape::new(sbl..sbr, 0.0..cap).stroked(stw))
         .build()
+}
+
+struct XShape {
+    xr: Range<f64>,
+    yr: Range<f64>,
+}
+
+impl XShape {
+    fn new(xr: Range<f64>, yr: Range<f64>) -> Self {
+        Self { xr, yr }
+    }
+}
+
+impl IntoOutlines for XShape {
+    type Outlines = [Arc<OutlineExpr>; 2];
+
+    fn into_outlines(self) -> Self::Outlines {
+        let slash = Slash::new(self.xr, self.yr).with_aln(SlashAlignment::symm(0.2));
+        let backslash = slash.clone().back();
+
+        [slash.into_outline(), backslash.into_outline()]
+    }
 }
