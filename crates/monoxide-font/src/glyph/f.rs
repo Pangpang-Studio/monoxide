cdb3ba2f5c66270f85314cb2ef99d09b834d93e7
rami3l <rami3l@outlook.com>
2026-06-07 22:01:11 +0200

    feat(font/glyph/f): add `f_cap`

--- crates/monoxide-font/src/glyph/f.rs
+++ crates/monoxide-font/src/glyph/f.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{ops::Range, sync::Arc};
 
 use monoxide_script::prelude::*;
 
@@ -7,6 +7,16 @@ use crate::{InputContext, dir::Alignment, glyph::j::JShape, math::mix, prelude::
 pub fn f(cx: &InputContext) -> Glyph {
     Glyph::builder()
         .outlines(FShape::from_settings(&cx.settings))
+        .build()
+}
+
+pub fn f_cap(cx: &InputContext) -> Glyph {
+    let FontParamSettingsView {
+        stw, sbl, sbr, cap, ..
+    } = cx.settings().view();
+
+    Glyph::builder()
+        .outlines(FCapShape::new(sbl + stw / 2.0..sbr, 0.0..cap).stroked(stw))
         .build()
 }
 
@@ -49,5 +59,33 @@ impl IntoOutlines for FShape {
 
     fn into_outlines(self) -> Self::Outlines {
         [self.hook, self.crossbar].map(move |it| it.transformed(Affine2D::translated(self.offset)))
+    }
+}
+
+pub struct FCapShape {
+    pub pipe: Rect,
+    pub top: Rect,
+    pub crossbar: Rect,
+}
+
+impl FCapShape {
+    pub fn new(xr: Range<f64>, yr: Range<f64>) -> Self {
+        let Range { start: x0, end: x1 } = xr;
+        let Range { start: y0, end: y1 } = yr;
+        let y = y0.midpoint(y1);
+
+        Self {
+            pipe: Rect::new((x0, y0), (x0, y1)).aligned(Alignment::Left),
+            top: Rect::new((x0, y1), (x1, y1)).aligned(Alignment::Left),
+            crossbar: Rect::new((x0, y), (mix(x1, x0, 0.85), y)),
+        }
+    }
+}
+
+impl IntoOutlines for FCapShape {
+    type Outlines = [Arc<OutlineExpr>; 3];
+
+    fn into_outlines(self) -> Self::Outlines {
+        [self.pipe, self.top, self.crossbar].map(|it| it.into_outline())
     }
 }
