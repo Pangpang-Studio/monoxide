86689a0cbf7a6d23b9082d7b9e7361fedb847568
rami3l <rami3l@outlook.com>
2026-06-08 21:43:35 +0200

    refactor(font/glyph/k): extract `Chevron`

--- crates/monoxide-font/src/glyph/k.rs
+++ crates/monoxide-font/src/glyph/k.rs
@@ -1,8 +1,11 @@
+use std::{ops::Range, sync::Arc};
+
 use monoxide_script::prelude::*;
 
 use crate::{
     InputContext,
     dir::{Alignment, Dir},
+    math::mix,
     prelude::*,
     shape::Rect,
 };
@@ -15,34 +18,52 @@ pub fn k(cx: &InputContext) -> Glyph {
         mih,
         stw,
         cap,
-        lower_right,
         ..
     } = cx.settings().view();
 
-    let k_mid_offset = Point2D::new(sbr - sbl, 0.) * 0.1;
+    let pipe = Rect::new((sbl + stw / 4., 0.), (sbl + stw / 4., cap)).aligned(Alignment::Left);
+    let chevron = Chevron::new(mix(sbr, sbl, 0.1) + stw..sbr, 0.0..xh);
+    let bar = Rect::new((sbl + stw / 4., mih), chevron.corner() + (stw, 0.).into());
 
-    let pipe = Rect::new((sbl + stw / 4., 0.), (sbl + stw / 4., cap))
-        .aligned(Alignment::Left)
-        .stroked(stw)
-        .into_outline();
-
-    let xh_right = Point2D::new(sbr, xh);
-    let mid = Point2D::new(sbl + stw, mih) + k_mid_offset;
-
-    let chevron = SpiroBuilder::open()
-        .insts([
-            corner!(lower_right)
-                .aligned(Alignment::Right)
-                .heading(Dir::U),
-            corner!(mid).aligned(Alignment::Left),
-            corner!(xh_right).aligned(Alignment::Right).heading(Dir::U),
+    Glyph::builder()
+        .outlines([
+            pipe.stroked(stw).into_outline(),
+            // TODO: Find out how the scaling factors are determined.
+            chevron.stroked(stw * 0.8),
+            bar.stroked(stw * 0.9).into_outline(),
         ])
-        // TODO: Find out how the scaling factor is determined.
-        .stroked(stw * 0.8);
+        .build()
+}
 
-    let bar = Rect::new(mid.with_x(sbl + stw / 4.), mid + (stw, 0.).into())
-        .stroked(0.9 * stw)
-        .into_outline();
+#[derive(Clone, Debug)]
+pub struct Chevron {
+    pub xr: Range<f64>,
+    pub yr: Range<f64>,
+}
 
-    Glyph::builder().outlines([pipe, chevron, bar]).build()
+impl Chevron {
+    pub fn new(xr: Range<f64>, yr: Range<f64>) -> Self {
+        Self { xr, yr }
+    }
+
+    pub const fn corner(&self) -> Point2D {
+        let Range { start: y0, end: y1 } = self.yr;
+        Point2D::new(self.xr.start, y0.midpoint(y1))
+    }
+}
+
+impl IntoOutline for Chevron {
+    fn into_outline(self) -> Arc<OutlineExpr> {
+        let Range { start: x0, end: x1 } = self.xr;
+        let Range { start: y0, end: y1 } = self.yr;
+
+        SpiroBuilder::open()
+            .insts([
+                corner!(x1, y0).aligned(Alignment::Right).heading(Dir::U),
+                corner!(x0, y0.midpoint(y1)).aligned(Alignment::Left),
+                corner!(x1, y1).aligned(Alignment::Right).heading(Dir::D),
+            ])
+            .build()
+            .into_outline()
+    }
 }
