fc6df1a74dbafde89480e87a8785404f250c4e5f
rami3l <rami3l@outlook.com>
2026-06-20 19:01:21 +0200

    feat(font/glyph/digit): add `nine`

--- crates/monoxide-font/src/glyph/digit.rs
+++ crates/monoxide-font/src/glyph/digit.rs
@@ -1,6 +1,7 @@
 mod four;
+mod nine;
 mod one;
 mod seven;
 mod zero;
 
-pub use self::{four::four, one::one, seven::seven, zero::zero};
+pub use self::{four::four, nine::nine, one::one, seven::seven, zero::zero};
