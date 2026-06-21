c8711eba729b1b0448cb77ef0e571adac35cf40b
rami3l <rami3l@outlook.com>
2026-06-21 09:53:31 +0200

    feat(main)!: propagate errors to toplevel

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
-use monoxide_font::make_font;
-use monoxide_script::eval::AuxiliarySettings;
+use std::fs::File;
 
-fn main() {
+use monoxide_font::make_font;
+use monoxide_script::eval;
+use tracing_subscriber::filter::{EnvFilter, LevelFilter};
+
+fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
-        .with_env_filter(
-            tracing_subscriber::EnvFilter::from_default_env()
-                .add_directive(tracing_subscriber::filter::LevelFilter::INFO.into()),
-        )
+        .with_env_filter(EnvFilter::from_default_env().add_directive(LevelFilter::INFO.into()))
         .init();
     eprintln!("Hello from Monoxide!");
 
-    let res = monoxide_script::eval::eval(
+    let res = eval::eval(
         &make_font(),
-        &AuxiliarySettings {
+        &eval::AuxiliarySettings {
             point_per_em: 2048,
             font_name: "Monoxide".into(),
         },
-    )
-    .expect("Failed to evaluate font context");
+    )?;
+
     let fout = "out.ttf";
-    res.write(std::fs::File::create(fout).expect("Failed to open file"))
-        .expect("Failed to write font");
+    res.write(File::create(fout)?)?;
     eprintln!("Successfully generated '{fout}'");
+
+    Ok(())
 }
